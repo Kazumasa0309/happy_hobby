dba0ca8531c8c6cfa52160102a11e9dfc512acbf
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-07 14:31:43 +0900

    messageテーブルのアソシエーション設定

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,6 @@
 class Community < ApplicationRecord
   has_many :users_communities
   has_many :users, through: :users_communities
+  has_many :messages
   has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
 
   has_many :users_communities
   has_many :communities, through: :users_communities
+  has_many :messages
 end
