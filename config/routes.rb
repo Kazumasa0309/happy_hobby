9f16292069bd92b047508576515738d5816db91d
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-07 18:19:48 +0900

    チャット機能実装＆CSSファイル整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'communities/index'
   root to: "communities#index"
-  resources :communities, only: [:new, :create]
+  resources :communities, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
