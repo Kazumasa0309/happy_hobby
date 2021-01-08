2d48bf685e0095225c7779284988a771e8ab1666
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-08 16:42:55 +0900

    詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'communities/index'
   root to: "communities#index"
-  resources :communities, only: [:new, :create] do
+  resources :communities, only: [:new, :create, :show] do
     resources :messages, only: [:index, :create]
   end
 end
