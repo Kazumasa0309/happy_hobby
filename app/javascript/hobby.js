f1a0ea75c81be83106bad25ed7487de4e61b3303
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-14 14:55:22 +0900

    クリックアクションid変更

--- app/javascript/hobby.js
+++ app/javascript/hobby.js
@@ -1,15 +1,17 @@
 function move() {
   let question = document.getElementById('question');
+  let answer_left = document.getElementById('answer_left');
+  let answer_right = document.getElementById('answer_right');
   let img01 = document.getElementById('img01');
   let img02 = document.getElementById('img02');
 
-  img01.addEventListener('click', function() {
+  answer_left.addEventListener('click', function() {
     question.innerHTML = "質問1"
     img01.innerHTML = "<img src ='assets/guitar_stand.png', class='image'>";
     img02.innerHTML = "<img src ='assets/guitar_stand.png', class='image'>";
   });
   
-  img02.addEventListener('click', function() {
+  answer_right.addEventListener('click', function() {
     question.innerHTML = "質問2"
     img01.innerHTML = "<img src ='assets/guitar_stand.png', class='image'>";
     img02.innerHTML = "<img src ='assets/guitar_stand.png', class='image'>";
