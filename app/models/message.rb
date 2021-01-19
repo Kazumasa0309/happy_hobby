e5eaddc8a222219a581d543fe2db1c4538538779
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-19 16:30:12 +0900

    messageモデルのバリデーション

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :community
   belongs_to :user
   has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
