c2a6739cadf9fd200b42a2a50e6ab084ed37a548
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2021-01-19 16:27:23 +0900

    communityモデルのバリデーション設定

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,4 +3,11 @@ class Community < ApplicationRecord
   has_many :users, through: :users_communities
   has_many :messages
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :category
+    validates :profile
+    validates :image
+  end
 end
